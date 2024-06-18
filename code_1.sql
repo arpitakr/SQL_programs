6fcff078b1f03bef277b474bc0ab300e770be773
arpitakr <arpita2161994@gmail.com>
2024-06-18 17:00:33 +0530

    upadtes

--- code_1.sql
+++ code_1.sql
@@ -1,6 +1,5 @@
 -- From the IMDb dataset, print the title and rating of 
 --those movies which have a genre starting from 'C' with a budget higher than 4 Crore.
-
 SELECT Title,Rating
 FROM IMDB;
 INNER JOIN genre
